014e19ba102db087687b81e25bff8308b9dcea58
Jesica Sieiro <jesica.sieiro@gmail.com>
2022-05-24 20:59:15 -0300

    incoorporacion de icono en boton

--- src/components/CarWisget/CardWidget.js
+++ src/components/CarWisget/CardWidget.js
@@ -1,13 +1,13 @@
-/* 
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'; */
+
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 import Button from '@mui/material/Button';
 
 
 const CardWidget=()=>{
     return(
         <div>
-            {/* <AddShoppingCartIcon /> */}
-            <Button color="inherit">Comprar</Button>
+            
+            <Button color="inherit"><ShoppingCartRoundedIcon /></Button>
         </div>
     )
 }
