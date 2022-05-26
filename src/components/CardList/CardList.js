5f3c114d8c492af91041363e9cc8fe3d331b85c5
Jesica Sieiro <jesica.sieiro@gmail.com>
2022-05-26 20:01:13 -0300

    desafio promise

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,36 +1,18 @@
 import CardItem from '../CardItem/CardItem'
 import Grid from '@mui/material/Grid';
-const CardList=(productos)=>{
- /*    const listProductos= Object.values(productos).map((item)=>{
-        console.log(item)
-        return(
-            item.forEach(element => {
-                <Grid item md={3} key={element.id}>
-                    <CardItem title={element.title} price={element.price} imagen={element.imagen} stok={element.stok}></CardItem>
-                 </Grid>
-            })
-        
-        )
-    }); */
+const CardList=({productos})=>{
     return(
        
             <Grid container spacing={4}>
                 {
-                    Object.values(productos).map((item)=>{
+                  productos.map(({id,title,price,imagen,stok})=>{
                         return(
-                            /* item.forEach(element => {
-                                <Grid item md={3} key={element.id}>
-                                    <CardItem title={element.title} price={element.price} imagen={element.imagen} stok={element.stok}></CardItem>
-                                 </Grid>
-                            })
-                         */
                         
-                        item.map((prod)=>{
-                            {console.log(prod)}
-                            <Grid item md={3} key={prod.id}>
-                                    <CardItem title={prod.title} price={prod.price} imagen={prod.imagen} stok={prod.stok}></CardItem>
+                           
+                            <Grid item md={3} key={id}>
+                                    <CardItem title={title} price={price} imagen={imagen} stok={stok}></CardItem>
                             </Grid>
-                        })
+                        
                         )
                     })
                 }
